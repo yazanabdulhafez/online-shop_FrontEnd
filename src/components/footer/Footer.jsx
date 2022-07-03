696c177569376824e4a9c0e1f86e7bd0015eb1dc
yazanabdulhafez <yazanali31@yahoo.com>
2022-07-03 17:41:49 +0300

    add the add new item feature

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import {FaFacebookF} from 'react-icons/fa'
-import {FiInstagram} from 'react-icons/fi'
-import {IoLogoTwitter} from 'react-icons/io'
-import './Footer.css'
+import React from 'react';
+import {FaFacebookF} from 'react-icons/fa';
+import {FiInstagram} from 'react-icons/fi';
+import {IoLogoTwitter} from 'react-icons/io';
+import { useAuth0 } from '@auth0/auth0-react';
+import './Footer.css';
 
 const Footer = () => {
+
+  const {isAuthenticated} = useAuth0();
+
   return (
     <footer>
       <a href="#f" className='footer_logo'>Online Shop</a>
     <ul className='permaLinks'>
 <li><a href="#home">Home</a></li>
-<li><a href="#favorite">Favorite</a></li>
-<li><a href="#cart">Cart</a></li>
+{isAuthenticated && <li><a href="#favorite">Favorite</a></li>}
+{isAuthenticated && <li><a href="#cart">Cart</a></li>}
 <li><a href="#contact">Contact</a></li>
     </ul>
 
