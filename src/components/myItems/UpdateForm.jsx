57556f40f978b09397e7f6a494220fd0f9f8a6d9
yazanabdulhafez <yazanali31@yahoo.com>
2022-07-09 11:18:49 +0300

    restyle the header and the footer

--- src/components/myItems/UpdateForm.jsx
+++ src/components/myItems/UpdateForm.jsx
@@ -23,36 +23,36 @@ const UpdateForm = (props) => {
 
               <Form.Group className="mb-3" controlId="formBasicID">
                 <Form.Label>Id</Form.Label>
-                <Form.Control type="number" placeholder={props.element.id} onChange={(e) => props.id(e)} />
+                <Form.Control type="number" defaultValue={props.element.id} onChange={(e) => props.id(e)} />
 
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicTitle">
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder={props.element.title} onChange={(e) => props.title(e)} />
+                <Form.Control type="text" defaultValue={props.element.title} onChange={(e) => props.title(e)} />
 
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPrice">
                 <Form.Label>Price</Form.Label>
-                <Form.Control type="number" placeholder={props.element.price} onChange={(e) => props.price(e)} />
+                <Form.Control type="number" defaultValue={props.element.price} onChange={(e) => props.price(e)} />
 
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicDescription">
                 <Form.Label>Description</Form.Label>
-                <Form.Control as="textarea" rows={3} placeholder={props.element.description} onChange={(e) => props.description(e)} />
+                <Form.Control as="textarea" rows={3} defaultValue={props.element.description} onChange={(e) => props.description(e)} />
 
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicCategory">
                 <Form.Label>Category</Form.Label>
-                <Form.Control type="text" placeholder={props.element.category} onChange={(e) => props.category(e)} />
+                <Form.Control type="text" defaultValue={props.element.category} onChange={(e) => props.category(e)} />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicImage">
                 <Form.Label>Image</Form.Label>
-                <Form.Control type="text" placeholder={props.element.image} onChange={(e) => props.image(e)} />
+                <Form.Control type="text" defaultValue={props.element.image} onChange={(e) => props.image(e)} />
 
               </Form.Group>
 
