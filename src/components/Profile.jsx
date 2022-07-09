a51a0c0c6ea5bddc8313b07588fb2bca2f23b040
yazanabdulhafez <yazanali31@yahoo.com>
2022-07-09 11:42:47 +0300

    change the profile style

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Card } from "react-bootstrap";
+import '../App.css';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
- console.log(user);
+  console.log(user);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} style={{width: '20rem',height: '20rem',margin:'10px auto'}} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      <div className="profile_card">
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={user.picture} alt={user.name} />
+          <Card.Body>
+            <Card.Title>{user.name}</Card.Title>
+            <Card.Text>
+              {user.email}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     )
   );
